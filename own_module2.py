740c034c152ab8bf456a125f808a4b89ce26e8dc
Corazon0D <korazon@gmail.com>
2025-10-07 13:24:59 +0300

    Own modules and packages

---
+++ own_module2.py
@@ -0,0 +1,10 @@
+# Собственные модули
+# Их создание и подключение
+# Подключаем нужную функцию из пакета модулей (my_package)
+from my_package import hello, goodbye
+
+# my_package.hello()
+# my_package.goodbye()
+
+hello()
+goodbye()
